fc89adbd756485c84cd08b721070e0f3e1c08fe3
연하영 <dusgkdud7@naver.com>
2023-11-23 04:43:05 +0900

    챗봇 기본 질문 api

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -1,16 +1,22 @@
-// in ActionProvider.jsx
 import React from 'react';
+import axios from 'axios';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
   const handleHello = () => {
-    const botMessage = createChatBotMessage('Hello. Nice to meet you.');
-
-    setState((prev) => ({
-      ...prev,
-      messages: [...prev.messages, botMessage],
-    }));
+    for (let i = 1; i <= 5; i++) {
+      axios.get(`http://ceprj.gachon.ac.kr:60014/chat/basic-question?index=${i}`)
+        .then((response) => {
+          const botMessage = createChatBotMessage(response.data);
+          setState((prev) => ({
+            ...prev,
+            messages: [...prev.messages, botMessage],
+          }));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
-
 
   return (
     <div>
