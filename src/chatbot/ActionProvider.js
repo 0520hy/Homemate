bccfbef247373876c9de300b988ec786f0563622
연하영 <dusgkdud7@naver.com>
2023-11-23 04:45:34 +0900

    챗봇 질문 test

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-  const handleHello = () => {
-    for (let i = 1; i <= 5; i++) {
-      axios.get(`http://ceprj.gachon.ac.kr:60014/chat/basic-question?index=${i}`)
-        .then((response) => {
-          const botMessage = createChatBotMessage(response.data);
-          setState((prev) => ({
-            ...prev,
-            messages: [...prev.messages, botMessage],
-          }));
-        })
-        .catch((error) => {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      for (let i = 1; i <= 5; i++) {
+        try {
+          const response = await axios.get(`http://ceprj.gachon.ac.kr:60014/chat/basic-question?index=${i}`);
+          setQuestions(prevQuestions => [...prevQuestions, response.data]);
+        } catch (error) {
           console.log(error);
-        });
+        }
+      }
+    };
+    fetchQuestions();
+  }, []);
+
+  const handleHello = () => {
+    if (questions.length === 0) {
+      return;
     }
+
+    const botMessage = createChatBotMessage(questions[currentQuestionIndex]);
+
+    setState(prev => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+
+    setCurrentQuestionIndex(prevIndex => prevIndex + 1);
   };
 
   return (
--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -2,7 +2,7 @@ import { createChatBotMessage } from 'react-chatbot-kit';
 import ChatbotHeader from './ChatbotHeader';
 
 const config = {
-  initialMessages: [createChatBotMessage(`안녕하세요! HomeMate 챗봇입니다. 아래 기본 질문 5개에 대답해주세요!`)],
+  initialMessages: [createChatBotMessage(`안녕하세요. HomeMate 챗봇입니다. 아래 기본 질문 5개에 대답해주세요!`)],
   customComponents: {
    
    header: (props) => <ChatbotHeader onCloseClick={props.onCloseClick} />,
