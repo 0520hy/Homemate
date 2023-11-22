3685e52b6523dee8134e31d7ec02478679ebd438
연하영 <dusgkdud7@naver.com>
2023-11-22 23:31:00 +0900

    test

--- src/pages/CreateArticle.js
+++ src/pages/CreateArticle.js
@@ -43,76 +43,73 @@ export default function CreateArticle() {
     }
   };
 
-
-
-
   return (
     <>
-    <Grid item>
-    <ArrowBackIcon
-    onClick={handleGoBack}
-    style={{ fontSize: 50, color: '#4F4E4E', cursor: 'pointer' }} />
-  </Grid>
-    <Container>
-      <Grid container alignItems="center" justifyContent="flex-start" marginTop="20px">
-      </Grid>
-      <Grid container alignItems="center" justifyContent="center" marginTop="40px">
-        <Grid item>
-          <AssignmentTwoToneIcon style={{ color: '#1976d2', fontSize: 60, marginRight: '10px' }} />
+      <Container>
+        <Grid container alignItems="center" justifyContent="flex-start" marginTop="20px">
+          <Grid item>
+            <ArrowBackIcon
+              onClick={handleGoBack}
+              style={{ fontSize: 50, color: '#4F4E4E', cursor: 'pointer' }}
+            />
+          </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant="h4" style={{ fontWeight: 'bold', color: '#4F4E4E', display: 'inline-block' }}>
-            커뮤니티 글쓰기
-          </Typography>
+        <Grid container alignItems="center" justifyContent="center" marginTop="40px">
+          <Grid item>
+            <AssignmentTwoToneIcon style={{ color: '#1976d2', fontSize: 60, marginRight: '10px' }} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h4" style={{ fontWeight: 'bold', color: '#4F4E4E', display: 'inline-block' }}>
+              커뮤니티 글쓰기
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container justifyContent="center" marginTop="80px">
-        <Grid item  style={{ width: "80vw" }}>
-          <TextField
-            id="outlined-basic"
-            label="제목"
-            variant="outlined"
-            style={{ width: "100%", marginBottom: '10px' }}
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+        <Grid container justifyContent="center" marginTop="80px">
+          <Grid item  style={{ width: "80vw" }}>
+            <TextField
+              id="outlined-basic"
+              label="제목"
+              variant="outlined"
+              style={{ width: "100%", marginBottom: '10px' }}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Grid>
+          
+          <Grid item style={{ width: "80vw" }}>
+            <Divider style={{ margin: '20px 0' }} />
+            <TextField
+              id="outlined-basic"
+              label="내용을 입력하세요"
+              variant="outlined"
+              multiline
+              rows={20}
+              style={{ width: "100%", marginBottom: '10px' }}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </Grid>
         </Grid>
-        
-        <Grid item style={{ width: "80vw" }}>
-          <Divider style={{ margin: '20px 0' }} />
-          <TextField
-            id="outlined-basic"
-            label="내용을 입력하세요"
-            variant="outlined"
-            multiline
-            rows={20}
-            style={{ width: "100%", marginBottom: '10px' }}
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+        <Grid container justifyContent="center" marginTop="80px">
+          <Grid item>
+            <Button 
+              style={{ fontSize: "20px", width: "250px" }}
+              variant="contained" 
+              endIcon={<SendIcon />}
+              onClick={handleSubmit}
+            >
+              확인
+            </Button>
+          </Grid>
         </Grid>
-      
-      </Grid>
-      <Grid container justifyContent="center" marginTop="80px">
-        <Grid item>
-          <Button 
-            style={{ fontSize: "20px", width: "250px" }}
-            variant="contained" 
-            endIcon={<SendIcon />}
-            onClick={handleSubmit}
-          >
-            확인
-          </Button>
-        </Grid>
-      </Grid>
-      {showAlert && (
-        <Grid container justifyContent="center" marginTop="20px">
-          <Typography variant="body1" style={{ color: 'green' }}>
-            게시글이 작성되었습니다.
-          </Typography>
-        </Grid>
-      )}
-    </Container>
+        {showAlert && (
+          <Grid container justifyContent="center" marginTop="20px">
+            <Typography variant="body1" style={{ color: 'green' }}>
+              게시글이 작성되었습니다.
+            </Typography>
+          </Grid>
+        )}
+      </Container>
     </>
   );
 }
