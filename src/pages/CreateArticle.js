c0026c7873137561e2d9300f7abb48f759c91daa
연하영 <dusgkdud7@naver.com>
2023-11-22 22:40:38 +0900

    뒤로가기 버튼 추가

--- src/pages/CreateArticle.js
+++ src/pages/CreateArticle.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import SendIcon from '@mui/icons-material/Send';
 import AssignmentTwoToneIcon from '@mui/icons-material/AssignmentTwoTone';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Container, Grid, Typography, TextField, Divider, Button } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+
 
 export default function CreateArticle() {
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
   return (
-    <Container>
-      <Grid container alignItems="center" justifyContent="center" marginTop="80px">
+    <>
+ <Grid item>
+          <ArrowBackIcon 
+          style={{ fontSize: 50, color: '#4F4E4E', cursor: 'pointer' }} 
+          onClick={handleGoBack}/>
+        </Grid>
+    <Container> 
+      <Grid container alignItems="center" justifyContent="flex-start" marginTop="20px">
+      </Grid>
+      <Grid container alignItems="center" justifyContent="center" marginTop="40px">
         <Grid item>
           <AssignmentTwoToneIcon style={{ color: '#1976d2', fontSize: 60, marginRight: '10px' }} />
         </Grid>
@@ -16,7 +32,7 @@ export default function CreateArticle() {
           </Typography>
         </Grid>
       </Grid>
-      <Grid container justifyContent="center" marginTop="110px">
+      <Grid container justifyContent="center" marginTop="80px">
         <Grid item  style={{ width: "80vw" }}>
           <TextField
             id="outlined-basic"
@@ -27,7 +43,7 @@ export default function CreateArticle() {
         </Grid>
         
         <Grid item style={{ width: "80vw" }}>
-        <Divider style={{ margin: '20px 0' }} />
+          <Divider style={{ margin: '20px 0' }} />
           <TextField
             id="outlined-basic"
             label="내용을 입력하세요"
@@ -40,15 +56,16 @@ export default function CreateArticle() {
       
       </Grid>
       <Grid container justifyContent="center" marginTop="80px">
-      <Grid item>
-        <Button 
-        style={{fontSize: "20px", width: "250px"}}
-        variant="contained" 
-        endIcon={<SendIcon />}>
-          확인
-        </Button>
+        <Grid item>
+          <Button 
+            style={{ fontSize: "20px", width: "250px" }}
+            variant="contained" 
+            endIcon={<SendIcon />}
+          >
+            확인
+          </Button>
         </Grid>
-        </Grid>
-    </Container>
+      </Grid>
+    </Container></>
   );
 }
