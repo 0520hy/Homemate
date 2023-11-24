48eb4043b0651cc1d7be47b25aa2921fc62599ff
연하영 <dusgkdud7@naver.com>
2023-11-24 23:28:49 +0900

    무한 스크롤

--- src/pages/RealtyList.js
+++ src/pages/RealtyList.js
@@ -56,10 +56,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const LogoImage = styled('img')({
-  width: '200px',
-  margin: 'auto',
-});
 
 export default function RealtyList() {
   const [buildingList, setBuildingList] = useState([]); // 건물 목록 상태
@@ -67,35 +63,49 @@ export default function RealtyList() {
   const [itemsPerPage] = useState(20); // 페이지 당 아이템 수
   const [searchTerm, setSearchTerm] = useState(''); // 검색어 상태
   const [loading, setLoading] = useState(false); // 로딩 상태
-  const [showResults, setShowResults] = useState(false); // 검색 결과 표시 여부 상태
+  const [hasMore, setHasMore] = useState(true); // 더 많은 데이터가 있는지 여부 상태
   const navigate = useNavigate();
+ 
 
-  // 컴포넌트 마운트 후 건물 목록 가져오기
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const startIndex = (currentPage - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const response = await axios.get('http://ceprj.gachon.ac.kr:60014/building/getAll', {
-          params: {
-            startIndex,
-            endIndex,
-          },
-        });
-        setBuildingList(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+ // 컴포넌트 마운트 후 초기 데이터 가져오기
+ useEffect(() => {
+  fetchData();
+}, []);
+
+// 데이터 불러오기
+const fetchData = async () => {
+  try {
+    setLoading(true);
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const response = await axios.get('http://ceprj.gachon.ac.kr:60014/building/getAll', {
+      params: {
+        startIndex,
+        endIndex,
+      },
+    });
+    const newData = response.data;
+    if (newData.length === 0) {
+      setHasMore(false);
+    } else {
+      setBuildingList((prevData) => [...prevData, ...newData]);
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+    setLoading(false);
+  } catch (error) {
+    console.error(error);
+    setLoading(false);
+  }
+};
+
+// 스크롤 이벤트 핸들러
+const handleScroll = () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 20 && !loading && hasMore) {
     fetchData();
-  }, [currentPage, itemsPerPage]);
+  }
+};
 
-  // 현재 페이지 아이템
-  const getCurrentItems = () => {
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    return buildingList.slice(indexOfFirstItem, indexOfLastItem);
-  };
 
   // 건물 가격 텍스트
   const getPriceText = (building) => {
@@ -110,30 +120,28 @@ export default function RealtyList() {
     return `${building.transactionType} ${priceText}`;
   };
 
-  // 페이지 변경 핸들러
-  const handlePageChange = (event, page) => {
-    setCurrentPage(page);
-  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [loading, hasMore]);
 
   // 검색 api call
   const search = async () => {
     try {
-      setLoading(true);
       const response = await axios.get('http://ceprj.gachon.ac.kr:60014/building/search', {
         params: {
           keyword: searchTerm,
         },
       });
       setBuildingList(response.data);
-      setShowResults(true);
-      setLoading(false);
     } catch (error) {
       if (error.response && error.response.status === 404) {
         alert('검색 결과가 없습니다');
       } else {
         console.error(error);
+        
       }
-      setLoading(false);
     }
   };
 
@@ -141,14 +149,14 @@ export default function RealtyList() {
   const handleSearchKeyPress = (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
-      setLoading(true);
       search();
     }
   };
-
+  
   const handleGoBack = () => {
     navigate(-1);
   };
+  
 
   return (
     <>
@@ -159,34 +167,24 @@ export default function RealtyList() {
         />
       </Grid>
       <Grid container direction="column" justifyContent="center" alignItems="center">
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar elevation={0} style={{ backgroundColor: 'transparent' }} position="static">
-            <Grid item style={{ margin: '35px 50px 50px 50px' }}>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  style={{ color: '#898989' }}
-                  placeholder="지역명 또는 단지명으로 검색해주세요."
-                  inputProps={{ 'aria-label': 'search' }}
-                  value={searchTerm}
-                  onChange={(event) => setSearchTerm(event.target.value)}
-                  onKeyPress={handleSearchKeyPress}
-                />
-              </Search>
-            </Grid>
-          </AppBar>
-        </Box>
-        {showResults && buildingList.length > 0 ? (
-          <List sx={{ textAlign: 'center', width: '70%', margin: '50px', bgcolor: 'background.paper' }}>
-            {/* ... */}
-          </List>
-        ) : showResults && buildingList.length === 0 ? (
-          <Typography variant="h6" color="secondary" style={{ textAlign: 'center', marginTop: '50px' }}>
-            검색 결과가 없습니다.
-          </Typography>
-        ) : null}
+        {/* ... */}
+        <List sx={{ textAlign: 'center', width: '70%', margin: '50px', bgcolor: 'background.paper' }}>
+          {/* ... */}
+          {buildingList.map((building) => (
+            <React.Fragment key={building.id}>
+              <ListItem alignItems="center" onClick={() => { navigate(`/details/${encodeURIComponent(building.buildingName)}`) }}>
+                {/* ... */}
+              </ListItem>
+              <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
+              <Divider variant="inset" component="li" />
+            </React.Fragment>
+          ))}
+          {loading && (
+            <Typography variant="body2" color="text.secondary" style={{ textAlign: 'center', marginTop: '20px' }}>
+              로딩 중...
+            </Typography>
+          )}
+        </List>
       </Grid>
     </>
   );
