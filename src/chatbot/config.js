93b7ddea65eb4a359f7794166acc27ea06a7709c
연하영 <dusgkdud7@naver.com>
2023-11-24 11:39:16 +0900

    챗봇

--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -1,84 +1,19 @@
-import React from 'react';
-import axios from 'axios';
+import { createChatBotMessage } from 'react-chatbot-kit';
+import ChatbotHeader from './ChatbotHeader';
 
-const MessageParser = ({ children, actions }) => {
-  const questions = [
-    "아파트/빌라/원룸/오피스텔 중 원하는 공간 형태는 무엇인가요? 단어 형태로 답변해주세요:",
-    "매매/전세/월세 중 원하는 주거 형태는 무엇인가요? 단어 형태로 답변해주세요:",
-    "서울/성남 범위 이내 원하는 지역을 선택해주세요.(예시: 경기도 성남시 복정동) 만약 여러 지역을 원하시면 띄어쓰기로 구분해서 입력해주세요.",
-    "희망하는 가격대를 알려주세요(만원 단위로 입력) 월세의 경우 보증금/월세 형태로 입력해주세요:",
-    "원하시는 전용면적을 알려주세요. 단, 숫자만 입력해주세요",
-    "사용자님 맞춤형 매물을 추천해드릴게요! 추가로 원하는 조건을 문장으로 말씀해주세요. 없으면 '아니요'를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요",
-    "원하는 추가 조건이 더 있으신가요? 없으면 아니요를 입력해주세요"
-  ];
-
-  const [currentQuestionIndex, setCurrentQuestionIndex] = React.useState(0);
-  const [isQuestionVisible, setIsQuestionVisible] = React.useState(false);
-
-  const handleAnswer = (answer) => {
-    actions.handleQuestionAnswer(answer);
-
-    // 다음 질문으로 넘어감
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-    setIsQuestionVisible(false);
-  };
-
-  const parse = async (message) => {
-    if (message === '아니요') {
-      // 추가 조건이 없음을 처리하는 로직을 여기에 작성합니다.
-      actions.handleNoAdditionalConditions();
-      setIsQuestionVisible(false);
-    } else {
-      // 추가 조건이 있는 경우를 처리하는 로직을 여기에 작성합니다.
-      actions.handleAdditionalCondition(message);
-      setIsQuestionVisible(true);
-    }
-  };
-
-  const BasicQuestion = ({ question, onAnswer }) => {
-    return (
-      <div>
-        <p>{question}</p>
-        <input type="text" onChange={(e) => onAnswer(e.target.value)} />
-      </div>
-    );
-  };
-
-  React.useEffect(() => {
-    setIsQuestionVisible(true);
-  }, []);
-
-  return (
-    <div>
-      {isQuestionVisible ? (
-        <BasicQuestion
-          question={questions[currentQuestionIndex]}
-          onAnswer={handleAnswer}
-        />
-      ) : null}
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          parse: parse,
-          actions: {},
-        });
-      })}
-    </div>
-  );
+const config = {
+  initialMessages: [createChatBotMessage(`안녕하세요.\n사용자님 취향에 맞는 매물을 찾아드릴게요!\n우선 기본 조건 질문에 답해주세요.`)],
+  customComponents: {
+   
+   header: (props) => <ChatbotHeader onCloseClick={props.onCloseClick} />,
+    botAvatar: (props) => <img width='50px' src='/images/bot-avatar.png' {...props} />,
+  },
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: '#5D86EF',
+    },
+    
+  },
 };
 
-export default MessageParser;
+export default config;
