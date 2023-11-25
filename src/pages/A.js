79584b429a3a51d994e3c522cf00ae1866b7422b
연하영 <dusgkdud7@naver.com>
2023-11-25 14:53:09 +0900

    챗봇 테스트

--- src/pages/A.js
+++ src/pages/A.js
@@ -131,34 +131,34 @@ class A extends Component {
             id: 'review',
             component: <Review />,
             asMessage: true,
-            trigger: '11',
+            trigger: '12',
           },
           {
-            id: '11',
+            id: '12',
             message: '사용자님 맞춤형 매물을 추천해드릴게요! 추가로 원하는 조건을 하나씩 말씀해주세요. (예시: 근처에 편의점이 있었으면 좋겠어요!) 없으면 [아니요]를 입력해주세요 ',
             trigger: 'additional-conditions',
           },
           
-          // {
-          //   id: 'additional-conditions',
-          //   user: true,
-          //   validator: (value) => {
-          //     if (value !== '아니요') {
-          //       // 사용자가 "아니요"가 아닌 다른 입력을 했을 경우 "원하는 추가 조건"을 처리하는 로직을 추가하세요.
-          //       // 원하는 추가 조건을 처리하는 로직이 끝나면 다음 단계로 이동시키세요.
-          //       return true; // 다음 단계로 이동
-          //     } else {
-          //       return false; // 추가 조건 입력 종료
-          //     }
-          //   },
-          //   trigger: (value) => (value !== '아니요' ? 'additional-conditions' : 'wait-message'),
-          // },
-          // {
-          //   id: 'wait-message',
-          //   message: '잠시만 기다려주세요...',
-          //   delay: 1000, // 1초 후에 다음 단계로 이동
-          //   trigger: '11',
-          // },
+          {
+            id: 'additional-conditions',
+            user: true,
+            validator: (value) => {
+              if (value !== '아니요') {
+                // 사용자가 "아니요"가 아닌 다른 입력을 했을 경우 "원하는 추가 조건"을 처리하는 로직을 추가하세요.
+                // 원하는 추가 조건을 처리하는 로직이 끝나면 다음 단계로 이동시키세요.
+                return true; // 다음 단계로 이동
+              } else {
+                return false; // 추가 조건 입력 종료
+              }
+            },
+            trigger: (value) => (value !== '아니요' ? 'additional-conditions' : 'wait-message'),
+          },
+          {
+            id: 'wait-message',
+            message: '잠시만 기다려주세요...',
+            
+            
+          },
           
          
         ]}
