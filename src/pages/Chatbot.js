c4dce021c1d1fc5d2ca50541d75dedb61dabfb53
연하영 <dusgkdud7@naver.com>
2023-11-28 23:15:25 +0900

    floatingicon

--- src/pages/Chatbot.js
+++ src/pages/Chatbot.js
@@ -161,6 +161,9 @@ class Response extends Component {
     );
   }
 }
+const CustomIcon = () => (
+  <img src="/images/logo.png" style={{ width: '80px', height: '80px' }} alt="Chatbot Icon" />
+);
 
 class MyChatbot extends Component {
   render() {
@@ -170,7 +173,7 @@ class MyChatbot extends Component {
           floating
           botAvatar="/images/bot-avatar.png"
           userAvatar="/images/user-avatar.png"
-          floatingIcon="/logo512.png"
+          floatingIcon={<CustomIcon />}
           floatingStyle={{width:"100px", height:"100px"}}
           headerTitle="HOMEMATE CHATBOT"
           steps={[
