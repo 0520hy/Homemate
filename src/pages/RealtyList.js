eab52b013f5dcc0788829ada82d72ae659b1dd6b
연하영 <dusgkdud7@naver.com>
2023-11-28 23:44:26 +0900

    매물 리스트 테스트

--- src/pages/RealtyList.js
+++ src/pages/RealtyList.js
@@ -56,18 +56,12 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const LogoImage = styled('img')({
-  width: '200px',
-  margin: 'auto',
-});
 
 export default function RealtyList() {
   const [buildingList, setBuildingList] = useState([]); // 건물 목록 상태
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
   const [itemsPerPage] = useState(20); // 페이지 당 아이템 수
   const [searchTerm, setSearchTerm] = useState(''); // 검색어 상태
-  const [loading, setLoading] = useState(false); // 로딩 상태
-  const [showResults, setShowResults] = useState(false); // 검색 결과 표시 여부 상태
   const navigate = useNavigate();
  
 
@@ -117,36 +111,32 @@ export default function RealtyList() {
   };
 
   // 검색 api call
-const search = async () => {
-  try {
-    setLoading(true);
-    const response = await axios.get('http://ceprj.gachon.ac.kr:60014/building/search', {
-      params: {
-        keyword: searchTerm,
-      },
-    });
-    setBuildingList(response.data);
-    setShowResults(true);
-    setLoading(false);
-  } catch (error) {
-    if (error.response && error.response.status === 404) {
-      alert('검색 결과가 없습니다');
-    } else {
-      console.error(error);
-    }
-    setLoading(false);
-  }
-};
-
-   // 검색 입력 (엔터) 핸들러
-   const handleSearchKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      setLoading(true);
-      search();
+  const search = async () => {
+    try {
+      const response = await axios.get('http://ceprj.gachon.ac.kr:60014/building/search', {
+        params: {
+          keyword: searchTerm,
+        },
+      });
+      setBuildingList(response.data);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        alert('검색 결과가 없습니다');
+      } else {
+        console.error(error);
+        
+      }
     }
   };
 
+  // 검색 입력 (엔터) 핸들러
+  const handleSearchKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      search();
+    }
+  };
+  
   const handleGoBack = () => {
     navigate(-1);
   };
@@ -154,16 +144,15 @@ const search = async () => {
 
   return (
     <>
-      <Grid container alignItems="center" justifyContent="flex-start" margin="30px">
-        <ArrowBackIcon
-          onClick={handleGoBack}
-          style={{ fontSize: 50, color: '#4F4E4E', cursor: 'pointer' }}
-        />
-      </Grid>
+     <Grid container alignItems="center" justifyContent="flex-start" margin="30px">
+    <ArrowBackIcon
+    onClick={handleGoBack}
+    style={{ fontSize: 50, color: '#4F4E4E', cursor: 'pointer' }} />
+  </Grid>
       <Grid container direction="column" justifyContent="center" alignItems="center">
         <Box sx={{ flexGrow: 1 }}>
           <AppBar elevation={0} style={{ backgroundColor: 'transparent' }} position="static">
-            <Grid item style={{ margin: '35px 50px 50px 50px' }}>
+            <Grid item style={{ margin: '100px 50px 50px 50px' }}>
               <Search>
                 <SearchIconWrapper>
                   <SearchIcon />
@@ -180,67 +169,54 @@ const search = async () => {
             </Grid>
           </AppBar>
         </Box>
-        {buildingList.length > 0 ? (
-          <List sx={{ textAlign: 'center', width: '70%', margin: '50px', bgcolor: 'background.paper' }}>
-            <Grid style={{ margin: '20px', textAlign: 'left', fontWeight: 'bold', color: '#414141' }}>검색 결과</Grid>
-            <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
+        <List sx={{ textAlign: 'center', width: '70%', margin: '50px', bgcolor: 'background.paper' }}>
+          <Grid style={{ margin: '20px', textAlign: 'left', fontWeight: 'bold', color: '#414141' }}>검색 결과</Grid>
+          <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
 
-            {getCurrentItems().map((building) => (
-              <React.Fragment key={building.id}>
-                <ListItem alignItems="center" onClick={() => { navigate(`/details/${encodeURIComponent(building.buildingName)}`) }}>
-                  <ListItemAvatar>
-                    <img
-                      src={`https://palgongtea.s3.ap-northeast-2.amazonaws.com/imgs/${building.buildingName}/${building.buildingName}_1.jpg`}
-                      width="200px"
-                      style={{ margin: '5px' }}
-                      alt=""
-                    />
-                  </ListItemAvatar>
-                  <div style={{ margin: '30px' }}>
-                    <ListItemText
-                      primary={<Typography variant="h5" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{building.title}</Typography>}
+          {getCurrentItems().map((building) => (
+            <React.Fragment key={building.id}>
+              <ListItem alignItems="center" onClick={()=>{navigate(`/details/${encodeURIComponent(building.buildingName)}`)}}>
+               
+                <ListItemAvatar>
+                  <img
+                    src={`https://palgongtea.s3.ap-northeast-2.amazonaws.com/imgs/${building.buildingName}/${building.buildingName}_1.jpg`}
+                    width="200px"
+                    style={{ margin: '5px' }}
+                    alt=""
+                  />
+                </ListItemAvatar>
+                <div style={{ margin: '30px' }}>
+                  <ListItemText
+                    primary={<Typography variant="h5" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{building.title}</Typography>}
+                    
+                    secondary={
+                      <React.Fragment>
+                        <Typography>{building.address}</Typography>
+                        <Typography
+                          sx={{ display: 'inline' }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                          style={{ fontSize: '1.3em' }}
+                        >
+                          {getPriceText(building)}
+                        </Typography>
+                      
+                      </React.Fragment>
+                    }
+                  />
+                </div>
+                
+              </ListItem>
+              <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
+              <Divider variant="inset" component="li" />
+            </React.Fragment>
+          ))}
 
-                      secondary={
-                        <React.Fragment>
-                          <Typography>{building.address}</Typography>
-                          <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                            style={{ fontSize: '1.3em' }}
-                          >
-                            {getPriceText(building)}
-                          </Typography>
-
-                        </React.Fragment>
-                      }
-                    />
-                  </div>
-
-                </ListItem>
-                <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
-                <Divider variant="inset" component="li" />
-              </React.Fragment>
-            ))}
-
-            <Grid sx={{ justifyContent: 'center', marginTop: '20px' }}>
-              <Pagination count={Math.ceil(buildingList.length / itemsPerPage)} page={currentPage} onChange={handlePageChange} />
-            </Grid>
-          </List>
-        )  : (
-          showResults ? (
-            <Typography variant="h6" color="secondary" style={{ textAlign: 'center', marginTop: '50px' }}>
-              검색 결과가 없습니다.
-            </Typography>
-          ) : (
-            <Typography variant="h6" color="secondary" style={{ textAlign: 'center', marginTop: '50px' }}>
-              검색해주세요!
-            </Typography>
-          )
-        )}
-
-
+          <Grid sx={{ justifyContent: 'center', marginTop: '20px' }}>
+            <Pagination count={Math.ceil(buildingList.length / itemsPerPage)} page={currentPage} onChange={handlePageChange} />
+          </Grid>
+        </List>
       </Grid>
     </>
   );
